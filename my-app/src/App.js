1171da6b0d06e453fe8ca96975a6da3520cd1528
jegedeBarak <barak.jegede@cdpipelinedevshop.com>
2022-07-11 15:15:53 +0000

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+         
+         
+        >
+          Learn React
+          
+<Button variant="contained">Contained</Button>
+<Button variant="contained" Disabled>
+</Button>
+<Button variant="contained" href="https://www.lamborghini.com/en-en"></Button>
+<Button color ="primary"></Button>
+
+ </a>
+</header>
+</div>
+ )
+}
+
